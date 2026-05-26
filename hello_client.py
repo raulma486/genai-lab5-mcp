5692938de1473f4a58a943e15ae9ddb4ae5d5889
Facundo Davancens <207771518+facundodortuy@users.noreply.github.com>
2026-05-26 00:19:13 +0000

    ADD: cliente y servidor MCP para el primer ejercicio

---
+++ hello_client.py
@@ -0,0 +1,10 @@
+import asyncio
+from fastmcp import Client
+
+async def main()
+    client = Client("hello_server.py")
+    async with client
+        result = await client.call_tool("say_hello", {"name": "Facundo"})
+        print(result)
+
+async.run(main())
---
+++ hello_server.py
@@ -0,0 +1,11 @@
+from fastmcp import FastMCP
+
+mcp = FastMCP("HelloServer")
+
+@mcp.tool
+def say_hello(name: str) -> str:
+    """Retorna un saludo amistoso."""
+    return f"Hola, {name}!"
+
+if __name__ == "__main__":
+    mcp.run()
